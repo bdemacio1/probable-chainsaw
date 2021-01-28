acb7f26c9a1984b0bb7fa82027e161f23c58596a
Demacio <Bobby.Demacio@ontario.ca>
2021-01-28 05:45:52 -0500

    fixed tests

--- src/test/java/org/acme/rest/client/NativeAnimalsResourceIT.java
+++ src/test/java/org/acme/rest/client/NativeAnimalsResourceIT.java
@@ -3,7 +3,7 @@ package org.acme.rest.client;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeCountriesResourceIT extends CountriesResourceTest {
+public class NativeAnimalsResourceIT extends AnimalsResourceTest {
 
     // Execute the same tests but in native mode.
 }
